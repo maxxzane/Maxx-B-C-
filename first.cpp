1c3ccb79aeb3961a6707361f41ed7cc88d76d22e
maxxzane <maxxbierwirth10@gmail.com>
2024-09-18 20:27:31 -0500

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    // integer
+    int number;
+    unsigned int number2;
+
+    cout << "Please enter a number";
+    cin >> number;
+
+    cout << "You entered" << number << endl;
+
+    return 0;
+
+}
