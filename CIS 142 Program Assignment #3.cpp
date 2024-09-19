80c9bedc02da9b7cd9fb69e3e6a90297b3653e13
maxxzane <maxxbierwirth10@gmail.com>
2024-09-19 02:17:45 -0500

    Add files via upload

---
+++ CIS 142 Program Assignment #3.cpp
@@ -0,0 +1,15 @@
+//Maxx Bierwirth 9/19/2024 Copyright
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "n\tn^2" << endl;
+
+	for (int n = 1; n <= 10; ++n) {
+		cout << n << "\t" << n * n << endl;
+	}
+
+	return 0;
+}
